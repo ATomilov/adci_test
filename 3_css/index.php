899b6d8a013c41cb1bdd5ecf6c73774313f2bad0
Alexey Tomilov <tomilovlehach@mail.ru>
2018-06-28 22:56:17 +0600

    Add files to complete third task

---
+++ 3_css/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+  <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<body>
+  <div class="teaser-container">
+  	<div class="teaser-container-header">
+    	<div class="teaser-title">
+      	<h3>Recently Added Videos</h3>
+      </div>
+      <div class="teaser-navigation">
+      	<div class="arrow-left">
+        	<i class="fas fa-angle-left"></i>
+        </div>
+        <div class="pagination">
+          1 - 2
+        </div>
+        <div class="arrow-right">
+        	<i class="fas fa-angle-right"></i>
+        </div>
+      </div>
+    </div>
+    <div class="teaser-container-content">
+      <div class="teaser-card">
+        <div class="poster">
+          <div class="poster-image">
+            <img src="https://placeimg.com/640/400/any" alt="">
+          </div>
+          <div class="hint-background">
+            <div class="hint-text">
+            	Hover for details
+            </div>
+          </div>
+          <div class="teaser-description">
+            <div class="description-text">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, eius!
+            </div>
+          </div>
+        </div>
+        <div class="teaser-tags-container">
+          <div class="teaser-tags-content">
+            Lorem ipsum, dolor sit amet, consectetur, adipisicing elit, Laborum, quis
+          </div>
+        </div>
+      </div>
+      <div class="teaser-card">
+        <div class="poster">
+          <div class="poster-image">
+            <img src="https://placeimg.com/640/400/any" alt="">
+          </div>
+          <div class="hint-background">
+            <div class="hint-text">
+            	Hover for details
+            </div>
+          </div>
+          <div class="teaser-description">
+            <div class="description-text">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, eius!
+            </div>
+          </div>
+        </div>
+        <div class="teaser-tags-container">
+          <div class="teaser-tags-content">
+            Lorem ipsum, dolor sit amet, consectetur, adipisicing elit, Laborum, quis
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
